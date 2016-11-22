9709413ae5de8981d2f4e37c75ef84a3f322586c
Rickard Jonsson <me@rickardjonsson.se>
2016-11-22 16:24:43 +0100

    Created script to prepare files used for testing
    Storing list and source files would be unnecessary as they could easily
    be generated, removed existing list and instead let the setup script
    create a source file, create a list file using this source file (Depends
    on dw -c working properly.) and then using the resulting file to create
    list files with bad formatting.

---
+++ tests/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+. ./util.sh
+
+ID_L=5
+SRC_FILE=src_file.tmp
+LST_FILE=list.lst
+CHAR_S=0123456789
+CS_SIZE=`echo $CHAR_S | wc -m`
+CS_SIZE=`expr $CS_SIZE - 1`
+L_SIZE=1
+i=1
+while [[ i -le `expr ${ID_L}` ]]; do
+    L_SIZE=`expr $L_SIZE \* $CS_SIZE`
+    i=`expr $i + 1`
+done
+
+if [ -f $SRC_FILE ]; then
+    while true; do
+        read -p "$SRC_FILE already exists, delete? [y/n]" yn
+        case $yn in
+            [Yy]* ) rm $SRC_FILE; break;;
+            [Nn]* ) SRC_FILE_GEN=no; break;;
+        esac
+    done
+fi
+
+if [[ -z "$SRC_FILE_GEN" ]]; then
+    echo "Creating source file suitable for id length $ID_L and character set size $CS_SIZE($L_SIZE words)..."
+    i=0
+    while [[ i -le `expr ${L_SIZE} - 1` ]]; do
+        if [[ $i -ne 0 ]] && [[ `expr $i % 1000` -eq 0 ]]; then
+            echo "`expr $i / 1000`000 words complete."
+        fi
+        echo "word$i" >> $SRC_FILE
+        i=`expr $i + 1`
+    done
+    echo "Source file generation done."
+fi
+
+echo "Testing -g option of dw."
+
+# Ensure no prompt will happen during testing, will not be visible
+if [ -f $LST_FILE ]; then
+    rm $LST_FILE
+fi
+
+assert 'dw --create-list=${SRC_FILE} --use-list=$LST_FILE' 0 'LIST GENERATION - Complete file' 'gen.log'
+
+echo "Creating list files for testing purposes..."
+head -n25 $LST_FILE table_formatting/incomplete.lst
+tail -n +3 $LST_FILE table_formatting/missing_info.lst
+cp $LST_FILE invocation/list.lst
+cp $SRC_FILE invocation/src.tmp
