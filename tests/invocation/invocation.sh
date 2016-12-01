1e3105ae7aa5ac9cbc760d9db5497cbd66210e33
Rickard Jonsson <me@rickardjonsson.se>
2016-12-02 00:25:26 +0100

    Added invocation tests
    Added tests for character groups in config as well as memory allocation
    handling.

--- tests/invocation/invocation.sh
+++ tests/invocation/invocation.sh
@@ -14,4 +14,7 @@ assert 'echo "y" | $BIN -g5 --create-list=src.tmp --use-list=new_list.lst' 0 \
     'INVOCATION - List create and passphrase generate chaining 3' 'invocation_chaining-3.log'
 assert 'echo "42342 12345 83723" | $BIN --use-list=list.lst --lookup | grep "word24324"' 0 \
     'INVOCATION - Passphrase lookup' 'invocation_lookup.log'
-
+assert 'echo "y" | $BIN --create-list=src.tmp --use-list=c_groups.lst --use-config=c_groups.conf && cat c_groups.lst | grep "ajibe"' 0 \
+    'INVOCATION - Character set groups' 'invocation_cgroup.log'
+assert 'echo "y" | $BIN --create-list=src.tmp --use-list=alloc_err.lst --use-config=alloc_error.conf' 1 \
+    'INVOCATION - Memory allocation error' 'invocation_allocation-error.log'
