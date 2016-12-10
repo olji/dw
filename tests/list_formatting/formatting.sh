f0c5beda3a1663ecf700bc34ed30d7b7e41c6b58
Rickard Jonsson <me@rickardjonsson.se>
2016-12-10 04:54:10 +0100

    Updated test to not look for prefix ERR

--- tests/list_formatting/formatting.sh
+++ tests/list_formatting/formatting.sh
@@ -9,12 +9,12 @@ assert '$BIN --use-list=incomplete.lst -g5' \
     'LIST FORMAT - Entry completeness' \
     'lst-format_entry-complete.log'
 assert '$BIN --use-list=missing_info.lst -g5' \
-    'grep "ERR: Missing information" $OUTFILE' \
+    'grep "Missing information" $OUTFILE' \
     0 \
     'LIST FORMAT - Missing information' \
     'lst-format_missing-info.log'
 assert '$BIN --use-list=charset-len-wrong.lst -g5' \
-    'grep "ERR: Given character set" $OUTFILE' \
+    'grep "Given character set" $OUTFILE' \
     0 \
     'LIST FORMAT - Unequal character set length' \
     'lst-format_charset-len.log'
