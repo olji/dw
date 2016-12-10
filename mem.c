b4806d9d969d629c50991a9010a80ba92296d3ff
Rickard Jonsson <me@rickardjonsson.se>
2016-12-10 05:01:06 +0100

    Added wrapper function for allocation of strings
    Useful when allocating memory for strings where the memory state is
    irrelevant, but still want to have predicted behaviour when used with
    standard library functions.

--- mem.c
+++ mem.c
@@ -7,6 +7,12 @@ void *malloc_assert(size_t size){
     mem_assert(mem);
     return mem;
 }
+char *str_malloc(size_t size){
+    char *mem = malloc(sizeof(char) * (size + 1));
+    mem_assert(mem);
+    mem[size] = '\0';
+    return mem;
+}
 void *calloc_assert(size_t amount, size_t size){
     void *mem = calloc(amount, size);
     mem_assert(mem);
--- mem.h
+++ mem.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 void *malloc_assert(size_t size);
+char *str_malloc(size_t size);
 void *calloc_assert(size_t size, size_t amonut);
 void mem_assert(void* pointer);
 
