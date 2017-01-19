3ea8d35613e85dc237f9a6c18ecf79170380974d
Rickard Jonsson <me@rickardjonsson.se>
2017-01-19 14:48:20 +0100

    Removed echoes where currently possible
    The way the testing script works makes valgrind check for memory leaks
    from echo.
    When file support is added to --lookup echo should be removed there as
    well.

--- tests/invocation/tests.sh
+++ tests/invocation/tests.sh
@@ -20,19 +20,19 @@ assert '$BIN -g5 --use-list=list.lst' \
     'INVOCATION - Standard generation with --use-list: Passphrase words' \
     'invocation_use-list-word.log'
 # Chaining 1
-assert 'echo "y" | $BIN --create-list=src.tmp --use-list=invocation_new_list1.lst -g5' \
+assert '$BIN --create-list=src.tmp --use-list=invocation_new_list1.lst -g5' \
     'ls | grep invocation_new_list1.lst' \
     0 \
     'INVOCATION - List create and passphrase generate chaining 1' \
     'invocation_chaining-1.log'
 # Chaining 2
-assert 'echo "y" | $BIN --use-list=invocation_new_list2.lst -g5 --create-list=src.tmp' \
+assert '$BIN --use-list=invocation_new_list2.lst -g5 --create-list=src.tmp' \
     'ls | grep invocation_new_list2.lst' \
     0 \
     'INVOCATION - List create and passphrase generate chaining 2' \
     'invocation_chaining-2.log'
 # Chaining 3
-assert 'echo "y" | $BIN -g5 --create-list=src.tmp --use-list=invocation_new_list3.lst' \
+assert '$BIN -g5 --create-list=src.tmp --use-list=invocation_new_list3.lst' \
     'ls | grep invocation_new_list3.lst' \
     0 \
     'INVOCATION - List create and passphrase generate chaining 3' \
@@ -43,13 +43,13 @@ assert 'echo "4342 1345 8723" | $BIN --use-list=list.lst --lookup' \
     'INVOCATION - Passphrase lookup' \
     'invocation_lookup.log'
 # Character set usage
-assert 'echo "y" | $BIN --create-list=src.tmp --use-list=invocation_c_set.lst --use-config=c_set.conf' \
+assert '$BIN --create-list=src.tmp --use-list=invocation_c_set.lst --use-config=c_set.conf' \
     'grep "ajib" invocation_c_set.lst' \
     0 \
     'INVOCATION - Character set used properly' \
     'invocation_cset.log'
 # Allocation error
-assert 'echo "y" | $BIN --create-list=src.tmp --use-list=invocation_alloc_err.lst --use-config=alloc_error.conf' \
+assert '$BIN --create-list=src.tmp --use-list=invocation_alloc_err.lst --use-config=alloc_error.conf' \
     'grep "Allocation error" $OUTFILE' \
     0 \
     'INVOCATION - Memory allocation error' \
